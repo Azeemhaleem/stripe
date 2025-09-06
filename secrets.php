9da70e1ac3d6862e0bf362858da71957fa763393
Azeemhaleem <157594820+Azeemhaleem@users.noreply.github.com>
2025-09-06 12:09:27 +0530

    Add stripe sample project with all files

---
+++ secrets.php
@@ -0,0 +1,6 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+$stripeSecretKey = 'sk_test_51S3e78JxgyGSTjPPaNL7U04f0wepolUEkuqqLO1FakZ8iTpZ6b9XWDa1AF1BKfr1yeqmQ3l0O7YmfyYHRq54GB0300BWvnAEKy';
